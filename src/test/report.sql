040339ba13a553329ce1edb5623d347591251567
Matt Schell <mschell@doitt.nyc.gov>
2021-05-27 16:22:57 -0400

    adds tests for null suites or missing NG911 data

--- src/test/report.sql
+++ src/test/report.sql
@@ -1,4 +1,6 @@
-select 'Tuples (ap_id,melissa_suite,usps_hnum) missing from subaddress_add: ' from dual;
+select 'Tuples (ap_id,melissa_suite,usps_hnum) missing from subaddress_add: ' 
+from 
+    dual;
 select 
     ap_id
    ,UPPER(melissa_suite)
@@ -12,7 +14,9 @@ select
    ,usps_hnum
 from 
 subaddress_add;
-select 'Tuples (ap_id,melissa_suite,usps_hnum) that should not be in subaddress_add: ' from dual;
+select 'Tuples (ap_id,melissa_suite,usps_hnum) that should not be in subaddress_add: ' 
+from 
+    dual;
 select 
     ap_id
    ,UPPER(melissa_suite) 
@@ -26,7 +30,9 @@ select
    ,usps_hnum
 from 
 subaddress_add_test;
-select 'sub_address_ids missing from subaddress_delete: ' from dual;
+select 'sub_address_ids missing from subaddress_delete: ' 
+from 
+    dual;
 select 
     sub_address_id 
 from 
@@ -36,7 +42,9 @@ select
     sub_address_id 
 from 
     subaddress_delete;
-select 'sub_address_ids that should not be in subaddress_delete: ' from dual;
+select 'sub_address_ids that should not be in subaddress_delete: ' 
+from 
+    dual;
 select 
     sub_address_id 
 from 
@@ -46,5 +54,34 @@ select
     sub_address_id 
 from 
     subaddress;
-select 'Rows in subaddress_add with incorrect attributes: ' from dual;
--- TODO - other derived attributes tests here
+-- these existed in legacy data, make sure we dont add more
+-- NULL melissa suites or different mixed case versions of No Data
+select 'sub_address_ids with null or ''No Data'' melissa_suite:' 
+from 
+    dual;
+select 
+    sub_address_id
+from 
+    subaddress_add
+where 
+    melissa_suite is null
+or  upper(melissa_suite) LIKE '%NO DATA%';
+-- these also existed in legacy data. populated melissa_suite but
+-- all six NG911 compartmentalized information fields are NULL
+select 'sub_address_ids with no NG911 compartmentalized information fields:' 
+from 
+    dual;
+select 
+    sub_address_id 
+from 
+    subaddress_add
+where 
+    additional_loc_info is null
+and building is null
+and floor is null
+and unit is null
+and room is null
+and seat is null
+and melissa_suite is not null;
+
+
