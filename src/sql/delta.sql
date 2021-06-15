4b3862fb08c767c3d00bebf67960cb4c0fb3be0e
Matt Schell <mschell@doitt.nyc.gov>
2021-06-15 12:32:56 -0400

    corrects delete sql

--- src/sql/delta.sql
+++ src/sql/delta.sql
@@ -20,21 +20,37 @@ where not exists
      where a.suite = b.melissa_suite
      and   a.addresspointid = b.ap_id);
 commit; 
---Any remaining unmatched CSCL subaddresses for an address should be deleted
+-- Any remaining unmatched CSCL subaddresses for an address should be deleted
+-- (or written in work table form)
+-- For any source address point with some additions in subaddress_add
+-- From that source, subtract the melissa data to remove fully matched records 
+-- any remaining records in the source should be deleted
 insert into subaddress_delete (
     sub_address_id
-) select distinct
-    a.sub_address_id
-from
-    subaddress_src a
-join 
-    subaddress_add b
-on 
-    a.ap_id = b.ap_id
-where not exists
-    (select 1
-     from 
-         subaddress_add c
-     where a.melissa_suite = c.melissa_suite
-     and   a.ap_id = c.ap_id);
+) 
+select 
+    aa.sub_address_id 
+from 
+    subaddress_src aa
+join (select 
+          a.melissa_suite
+         ,a.ap_id
+         ,a.usps_hnum 
+      from 
+          subaddress_src a
+      where 
+          a.ap_id in (select 
+                          b.ap_id 
+                      from 
+                          subaddress_add b)
+      minus
+      select 
+          c.suite
+         ,c.addresspointid
+         ,c.hnum 
+      from 
+          melissa_geocoded_src c) bb
+on (aa.melissa_suite = bb.melissa_suite
+    and aa.ap_id = bb.ap_id
+    and aa.usps_hnum = bb.usps_hnum);
 commit;
